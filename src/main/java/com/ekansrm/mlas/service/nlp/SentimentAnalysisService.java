ekansrm/MLasS-mlas

情感分析Service 和 Controller, 与 xmlrpc 连接

src/main/java/com/ekansrm/mlas/service/nlp/SentimentAnalysisService.java
@@ -1,4 +1,9 @@
 package com.ekansrm.mlas.service.nlp;
 
+import org.apache.xmlrpc.XmlRpcException;
+
 public interface SentimentAnalysisService {
+
+  String predict(String text) throws XmlRpcException;
+
 }

src/main/java/com/ekansrm/mlas/service/nlp/impl/SentimentAnalysisServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ekansrm.mlas.service.nlp.impl;
 
 import com.ekansrm.mlas.service.nlp.SentimentAnalysisService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
@@ -15,6 +17,12 @@ import java.util.Arrays;
 @Service("sentimentAnalysisService")
 public class SentimentAnalysisServiceImpl implements SentimentAnalysisService {
 
+  static private Gson gson;
+  static {
+    gson = new GsonBuilder().setPrettyPrinting().create();
+  }
+  private XmlRpcClient client;
+
   @PostConstruct
   public void init() {
     XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
@@ -24,13 +32,13 @@ public class SentimentAnalysisServiceImpl implements SentimentAnalysisService {
       e.printStackTrace();
     }
     config.setEnabledForExtensions(Boolean.TRUE);
-    XmlRpcClient client = new XmlRpcClient();
+    client = new XmlRpcClient();
     client.setConfig(config);
     //注意此时构造的远程python的函数参数
     Object[] params = new Object[]{Boolean.TRUE, 123, 1.23, "abc"};
     try{
       Object[] res = (Object[]) client.execute("testType", params);
-      System.out.println(res);//注意访问java的object类型数据的方法
+      System.out.println(gson.toJson(res));
     }
     catch (XmlRpcException e11)
     {
@@ -38,4 +46,9 @@ public class SentimentAnalysisServiceImpl implements SentimentAnalysisService {
     }
   }
 
+  @Override
+  public String predict(String text) throws XmlRpcException {
+    Object[] params = new Object[]{text};
+    return (String) client.execute("predict", params);
+  }
 }

KAMI-Wei/MLaaS-mlas

src/main/java/com/ekansrm/mlas/service/nlp/SentimentAnalysisService.java
@@ -1,4 +1,9 @@
 package com.ekansrm.mlas.service.nlp;
 
+import org.apache.xmlrpc.XmlRpcException;
+
 public interface SentimentAnalysisService {
+
+  Double predict(String text) throws XmlRpcException;
+
 }

src/main/java/com/ekansrm/mlas/service/nlp/impl/SentimentAnalysisServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ekansrm.mlas.service.nlp.impl;
 
 import com.ekansrm.mlas.service.nlp.SentimentAnalysisService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
@@ -15,6 +17,12 @@ import java.util.Arrays;
 @Service("sentimentAnalysisService")
 public class SentimentAnalysisServiceImpl implements SentimentAnalysisService {
 
+  static private Gson gson;
+  static {
+    gson = new GsonBuilder().setPrettyPrinting().create();
+  }
+  private XmlRpcClient client;
+
   @PostConstruct
   public void init() {
     XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
@@ -24,13 +32,13 @@ public class SentimentAnalysisServiceImpl implements SentimentAnalysisService {
       e.printStackTrace();
     }
     config.setEnabledForExtensions(Boolean.TRUE);
-    XmlRpcClient client = new XmlRpcClient();
+    client = new XmlRpcClient();
     client.setConfig(config);
     //注意此时构造的远程python的函数参数
     Object[] params = new Object[]{Boolean.TRUE, 123, 1.23, "abc"};
     try{
       Object[] res = (Object[]) client.execute("testType", params);
-      System.out.println(res);//注意访问java的object类型数据的方法
+      System.out.println(gson.toJson(res));
     }
     catch (XmlRpcException e11)
     {
@@ -38,4 +46,9 @@ public class SentimentAnalysisServiceImpl implements SentimentAnalysisService {
     }
   }
 
+  @Override
+  public Double predict(String text) throws XmlRpcException {
+    Object[] params = new Object[]{text};
+    return (Double) client.execute("predict", params);
+  }
 }